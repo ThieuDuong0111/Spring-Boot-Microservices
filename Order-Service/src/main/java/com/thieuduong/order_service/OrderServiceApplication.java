f5780237dc9aca0592b411390618df246c0877de
thieu.duong <duongthieu1995@gmail.com>
2024-07-30 23:34:16 +0700

    add api-gateway

--- Order-Service/src/main/java/com/thieuduong/order_service/OrderServiceApplication.java
+++ Order-Service/src/main/java/com/thieuduong/order_service/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.thieuduong.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
